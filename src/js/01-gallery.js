516a42f83a5c4c74e3259aa4cae23f4e65c60241
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-23 20:11:05 +0200

    npm install simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,7 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
