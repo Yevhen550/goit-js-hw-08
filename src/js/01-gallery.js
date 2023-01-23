4a4376aa86ac07617d93d591212b2726fe334600
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-23 20:21:11 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,32 @@
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}"
+            />
+        </a>`
+  )
+  .join('');
+gallery.innerHTML = markup;
+
+gallery.addEventListener('click', handleClick);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+function handleClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  lightbox.open(element);
+}
