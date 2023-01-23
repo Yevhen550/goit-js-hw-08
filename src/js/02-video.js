73dd482a9e34fdfcbbeb913fec05a3391850c4c7
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-23 20:49:55 +0200

    all

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('[name="email"]');
+const inputMessage = document.querySelector('[name="message"]');
+
+let storageData;
+try {
+  storageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+} catch (error) {
+  console.log(`Local storage: ${error.name}`);
+  console.log(`Local storage: ${error.message}`);
+}
+if (storageData !== null) {
+  const { email, message } = storageData;
+  if (email !== undefined) {
+    inputEmail.value = email;
+  }
+  if (message !== undefined) {
+    inputMessage.value = message;
+  }
+} else {
+  storageData = {};
+}
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    if (event.target.name === 'email') {
+      storageData.email = event.target.value;
+    } else if (event.target.name === 'message') {
+      storageData.message = event.target.value;
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(storageData));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  if (inputEmail.value !== '' && inputMessage.value !== '') {
+    event.preventDefault();
+    console.log(storageData);
+    storageData = {};
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  } else {
+    alert('Fill up all fields!');
+  }
+});
